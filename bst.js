c13bd027e7ba52d1e55c3ee9b43ad3fa5466b92b
drldx <168914904+drldx@users.noreply.github.com>
2026-03-10 12:30:54 +0530

    update: buildTree function
    a function to return the root node of the tree
    prettyPrint function for visualising
    tree array is sorted and filtered out

--- bst.js
+++ bst.js
@@ -1,24 +1,43 @@
 class Node {
-  constructor(data, left, right) {
+  constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
     this.right = right;
   }
-
 }
 
 class Tree {
   constructor(array) {
-    this.array = array;
-    this.root = buildTree(array);
+    this.array = array.sort((a, b) => a - b).filter((item, index) => item !== array[index - 1]);
+    this.root = buildTree(this.array);
   }
 };
 
 function buildTree(array, start = 0, end = array.length - 1) {
-  let sortedArray = array.sort((a, b) => a - b).filter((item, index) => item !== array[index - 1]);
-  return sortedArray;
+
+  if (start > end) return null;
+
+  let mid = Math.floor((start + end) / 2);
+
+  let root = new Node(array[mid]);
+  root.left = buildTree(array, start, mid - 1);
+  root.right = buildTree(array, mid + 1, end);
+
+  return root;
 }
 
+//prettyPrint function for visualising;
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(tree.root)
+prettyPrint(tree.root);
 
