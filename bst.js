20eacec956ba836e24d5b24e06574d922d9c6764
drldx <168914904+drldx@users.noreply.github.com>
2026-03-11 15:10:09 +0530

    feat: includes method
    a method to check if the given value is true or not
    recursive call on the nodes based on the root value;

--- bst.js
+++ bst.js
@@ -11,12 +11,30 @@ class Tree {
     this.array = array.sort((a, b) => a - b).filter((item, index) => item !== array[index - 1]);
     this.root = buildTree(this.array);
   }
+
+  print(value, root = this.root) {
+    if (root === null) return;
+
+    this.print(value, root.left);
+    console.log(root.data);
+    this.print(value, root.right);
+  }
+
+  includes(value, root = this.root) {
+    if (root === null) return false;
+    if (root.data === value) return true;
+
+    if (value < root.data) {
+      return this.includes(value, root.left);
+    } else {
+      return this.includes(value, root.right);
+    }
+  }
 };
 
 function buildTree(array, start = 0, end = array.length - 1) {
 
   if (start > end) return null;
-
   let mid = Math.floor((start + end) / 2);
 
   let root = new Node(array[mid]);
@@ -40,4 +58,5 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 prettyPrint(tree.root);
+console.log(tree.includes(9));
 
